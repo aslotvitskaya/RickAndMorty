f5a33b4d21102206066ea43d725e0207bd2e40b7
Slotvitskaya <avadanzele@mail.ru>
2026-05-18 16:56:49 +0300

    Added characters info at the screen

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -9,13 +9,17 @@ import UIKit
 
 final class RMCharacterViewController: UIViewController {
     
-    private let characterListView = CharacterListView()
+    private let characterListView = RMCharacterListView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         title = "Characters"
+        setUpView()
+        }
+    
+    private func setUpView() {
         view.addSubview(characterListView)
         NSLayoutConstraint.activate([
             characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -23,5 +27,6 @@ final class RMCharacterViewController: UIViewController {
             characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
             characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
-        }
+    }
+    
     }
---
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellModel.swift
@@ -0,0 +1,44 @@
+//
+//  RMCharacterCollectionViewCellModel.swift
+//  RickAndMorty
+//
+//  Created by Ana on 16.05.2026.
+//
+
+import Foundation
+
+final class RMCharacterCollectionViewCellModel{
+    public let characterName: String
+    private let characterStatus: RMCharacterStatus
+    private let characterImageURL: URL?
+    
+    init(
+        characterName: String,
+        characterStatus: RMCharacterStatus,
+        characterImageURL: URL?
+    ){
+        self.characterName = characterName
+        self.characterStatus = characterStatus
+        self.characterImageURL = characterImageURL
+    }
+    
+    public var characterStatusText: String {
+            return characterStatus.rawValue
+        }
+    
+    public func fetchImage(completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url = characterImageURL else {
+            completion(.failure(URLError(.badURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
